a11c63c434054610a3c45e967c61f939affe151d
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-07-08 21:44:45 +0400

    final version

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const form=document.getElementById('form');
 const username=document.getElementById('name');
 const pass=document.getElementById('psw');
-const errorname=document.querySelector(".showmessagename");
-const errorpass=document.querySelector(".showmessagepass");
+const errorname=document.querySelector(".errormessagename");
+const errorpass=document.querySelector(".errormessagepass");
 
 
 form.addEventListener('submit', function(e){
@@ -15,14 +15,12 @@ form.addEventListener('submit', function(e){
   if (pass.value == ""){
     errorpass.textContent='Please Enter Password';
   }
-  if (pass.value && pass.value.length <8){
+  if (pass.value && (pass.value.length <8 || pass.value.length >20) ){
     errorpass.textContent='Password must be 8-20 character';
   }
-
-  if(username.value && pass.value && pass.value.length >8 ){
+  if(username.value && pass.value && pass.value.length >= 8 && pass.value.length <=20){
     errorpass.textContent='';
     errorname.textContent='';
-    alert("Hello "+ username.value)
+    setTimeout(function(){ alert("Hello "+ username.value) }, 10);
   }
- 
 });
